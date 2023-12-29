c06169a236b321128053c422ae777044029341aa
musthafa <musthafa.cp@kalvium.community>
2023-12-29 17:50:21 +0530

    updaated

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import UseReducer from './components/UseReducer'
+import UseState from './components/UseState'
+
+
+function App() {
+  return (
+    <>
+      <UseReducer/>
+      {/* <UseState/> */} 
+    </>
+  )
+}
+
+export default App
